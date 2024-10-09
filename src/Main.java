ec3c13ffee4975828799dcceb5d15ffd1c7e3b1c
Duduenri <eduess290799@gmail.com>
2024-10-09 14:56:08 -0300

    building movie catalog

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo");
-        }
+        System.out.println("Screen Match");
+        System.out.println("Movie: Top Gun: Maverick");
+
+        int yearOfRelease = 2022;
+        System.out.println("Year of Release: " + yearOfRelease);
+        boolean subscription = true;
+        double filmNote = 8.1;
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Top Gun Movie
+                Adventure movie with 80s heartthrob
+                Very good!
+                Release year
+                """ + yearOfRelease;
+        System.out.println(sinopse);
+
+        int classification = (int) (media /2);
+        System.out.println(classification);
     }
+}
